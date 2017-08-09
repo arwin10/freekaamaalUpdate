51ca8bfd5d5ab9811ed65786b8d6eb1aae1a5ec3
Arindam Sarkar -X (arinsark - TATA CONSULTANCY SERVICES LIMITED at Cisco) <arinsark@partnet.cisco.com>
2017-08-09 21:13:14 +0530

    fist commit freekaamaalsupdate

---
+++ src/DBConnectionUtil.java
@@ -0,0 +1,256 @@
+import java.sql.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class DBConnectionUtil {
+	
+	/******************Prod DB Details **********************/
+	
+	static String serverName = "us-cdbr-iron-east-03.cleardb.net";
+    static String mydatabase = "heroku_813d276cc465e94";
+    //  Database credentials
+	static final String USER = "b1162df0501833";
+	static final String PASS = "6423989b";
+	
+	/*************Dev DB Details *********************/
+//	static String serverName = "localhost";
+//    static String mydatabase = "hotinddeals_db";
+//    //  Database credentials
+//	static final String USER = "root";
+//	static final String PASS = "admin";
+
+	// JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    static final String DB_URL  = "jdbc:mysql://" + serverName + "/" + mydatabase; 
+    
+    String cmbCategory="22";
+	//String txtName="xxx";
+	String txtDesc="xxx";
+	String txtSize="NA";
+	
+	String frdStatus="N";
+	String LatestStatus="Y";
+	String PrmStatus="N";
+	String AvlStatus="Y";
+	String ItemCondition="New";
+	//String QntAvl="200";
+	String PrdfullDesc="NA";
+	String Brand="NA";
+	String Modelno="NA";
+	String Dimension="NA";
+	
+	String DispSize="NA";
+	String PrdFeatures="NA";
+	String PrdReviews="NA";
+	
+	String PostedBy="arwin10";
+	String ReleaseDate="2017-08-03";
+	String ReleaseTime="20:06:10";
+	//String DealDescp="xxx";
+	//String DealLink="http://www.amazon.com";
+	String StoreId="1";
+	//String StoreName="@Amazon";
+	
+	//String txtPrice="200";
+	String txtDiscount="100";
+	//String txtFinal="100";
+	Float dis;
+	int flag=0;
+	
+	String path2= "unnamed.png";
+	//String path1= "unnamed.png";
+	
+	String CatDesc="xxxx";
+	String CatPath="xxxx";
+	String CatMainName="xxxx";
+	int catFound=0;
+	int offerFound=0;
+	String offerId="0";
+	Connection conn = null;
+	Statement stmt = null;
+	Statement stmt1 = null;
+	Statement stmt2 = null;
+    Statement stmt3 = null;
+	
+	public int dbconnectSetup()
+	{
+		 try{
+		      //STEP 2: Register JDBC driver
+		      Class.forName(JDBC_DRIVER);
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to a selected database...");
+		      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		      System.out.println("Connected database successfully...");
+		      if(conn!=null)
+		      {
+		    	  return 1;
+		      }
+		    
+		 }
+		 
+		 catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		 }
+		return 0;
+		 
+	}
+	
+	public void dbClose()
+	{
+		try{
+	         if(stmt!=null)
+	            conn.close();
+	         
+	      }catch(SQLException se){
+	      }// do nothing
+	      try{
+	         if(conn!=null)
+	            conn.close();
+	      }
+	      catch(SQLException se){
+		         se.printStackTrace();
+		      }
+	      
+	}
+	
+
+
+	public void dbUpdate(String txtName,String StoreName,String txtFinal,String txtPrice,String path1,String DealLink,String QntAvl,String DealDescp,String cmbCategoryName) throws Exception
+	{
+		   
+		   try{
+		      
+		      //STEP 4: Execute a query
+		      System.out.println("Inserting records into the table...");
+		      stmt = conn.createStatement();
+		      
+		      String catselect="select CategoryId from Category_Master where CategoryName='"+cmbCategoryName+"'";
+		      ResultSet rs= stmt.executeQuery(catselect);
+		      
+		      while(rs.next())
+		      {   
+		    	  catFound=1;
+		    	  System.out.println("Category Already Exist."); 
+		    	  cmbCategory= rs.getString("CategoryId");
+		          
+		      }
+		    	  
+		      
+		      if(catFound==0)
+		      {   
+		    	  stmt1 = conn.createStatement();
+		    	  System.out.println("Category Not found.");
+		    	  String catinsert= "insert into Category_Master(CategoryName,Description,Image,MainCategoryName,ActiveStatus) values('"+cmbCategoryName+"','"+CatDesc+"','"+CatPath+"','"+CatMainName+"','Y')";
+			      flag=stmt1.executeUpdate(catinsert);
+			      if(flag>0)
+			      { System.out.println("Inserted Category Successfully!");
+			         flag=0;
+			      }
+			      else
+			      System.out.println("Category insertion Failed!"); 
+			      
+			      stmt2 = conn.createStatement();
+			      String catreselect="select CategoryId from Category_Master where CategoryName='"+cmbCategoryName+"'";
+			      ResultSet rs1= stmt2.executeQuery(catreselect);
+			      
+			    	  while(rs1.next())
+			    	  {	  
+			    	   cmbCategory= rs1.getString("CategoryId");
+			    	  }
+			      
+			      
+			      rs1.close();
+		      }
+		      
+		      
+		      String offerselect="select ItemId from Item_Master where ItemName='"+txtName+"'";
+		      ResultSet rs1= stmt.executeQuery(offerselect);
+		      
+		       
+		     
+		      while(rs1.next())
+		      {   
+		    	  offerFound=1;
+		    	  offerId= rs1.getString("ItemId");
+		    	  System.out.println("Offer Already Exist with Offer Id="+offerId); 
+		          
+		      }
+		      
+		      if(offerFound==0)
+		      { 
+		    	 String storeselect="select StoreId from Popular_Stores where StoreName='"+StoreName+"'";
+			      ResultSet rs2= stmt.executeQuery(storeselect);
+			      			     
+			      while(rs2.next())
+			      {   
+			    	  StoreId= rs2.getString("StoreId");
+			    	  System.out.println("Store Id="+StoreId); 
+			          
+			      }
+		    	 
+		    	  
+		    	  stmt3 = conn.createStatement();
+		      
+		        dis = (Float.parseFloat(txtPrice)-Float.parseFloat(txtFinal));
+		        txtDiscount=Float.toString(dis);
+		        String sql = "insert into Item_Master(CategoryId,ItemName,Description,Size,Image,PrdDescFile,Price,Discount,Total,FeaturedPrd,LatestPrd,PromotedPrd,ItemCondtion,AvalibiltyStatus,QuantityAvailable,ItemsFullDescription,Brand,ModelNo,ReleaseDate,DisplaySize,PrdFeatures,PrdReviews,PostedBy,ReleaseTime,DealDescription,DealLink,StoreId,PrdDimension,DealWebsite) values("+cmbCategory+",'"+txtName+"','"+txtDesc+"','"+txtSize+"','"+path1+"','"+path2+"',"+txtPrice+","+txtDiscount+","+txtFinal+",'"+frdStatus+"','"+LatestStatus+"','"+PrmStatus+"','"+ItemCondition+"','"+AvlStatus+"',"+QntAvl+",'"+PrdfullDesc+"','"+Brand+"','"+Modelno+"','"+ReleaseDate+"','"+DispSize+"','"+PrdFeatures+"','"+PrdReviews+"','"+PostedBy+"','"+ReleaseTime+"','"+DealDescp+"','"+DealLink+"',"+StoreId+",'"+Dimension+"','"+StoreName+"')";
+		        flag=stmt3.executeUpdate(sql);
+		        if(flag>0)
+		        System.out.println("Inserted Offer Successfully!");
+		        else
+		    	System.out.println("Offer insertion Failed!"); 
+		        
+		        rs2.close();
+		      }  
+		      
+    		    rs.close();
+    		       
+
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		    
+		      try{
+		       
+		         catFound=0;
+		         offerFound=0;
+		         flag=0;
+		      }catch(Exception e){
+		         e.printStackTrace();
+		      }//end finally try
+		   }//end try
+		   
+		
+	}
+	
+	public void scrollDown(WebDriver driver,int crDwn) throws Exception
+	{
+		JavascriptExecutor je = (JavascriptExecutor) driver;
+		je.executeScript("scroll(0,"+crDwn+");");
+	}
+	
+	public void implicitWait(int time,WebDriver driver) throws Exception
+	{
+		 driver.manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
+	}
+	
+	public WebElement getElementWithIndex(WebDriver driver, int index,String xpath)
+	{
+	     List<WebElement> elements = driver.findElements(By.xpath(xpath)); 
+	     return elements.get(index);
+	}
+	
+	
+}
---
+++ src/OfferUpdate.java
@@ -0,0 +1,246 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class OfferUpdate {
+
+	WebDriver driver;
+	DBConnectionUtil dbconnectionutil=new DBConnectionUtil();	;
+	String userId="arinsark";       /*CEC Id */
+	String password="xxxx";   /*CEC Password*/
+	String url="https://www.desidime.com/";
+	String dealName="xxxx";
+	String dealStore="xxxx";
+	String dealPrice="0";
+	String dealImage="xxxx";
+	String dealLink="xxxx";
+	String newDealLink="xxxx";
+	String dealMRP="0";
+	String dealViews="0";
+	String dealDescp="xxxx";
+	String dealCategory="xxxx";
+	ArrayList<String> latestDeals = new ArrayList<String>();
+	ArrayList<String> finalPriceDeals = new ArrayList<String>();
+	WebElement deal;
+	WebElement dealTime;
+	int scrollDown=300;
+
+	@BeforeClass
+	public void setUp(){ 
+
+		try {
+
+
+			System.setProperty("webdriver.firefox.bin", "C:\\FirefoxPortable45ESR\\FirefoxPortable.exe");	 
+			//System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+			driver=new FirefoxDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			dbconnectionutil.dbconnectSetup();
+
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception occured:"+e);
+			e.printStackTrace();
+		}
+
+
+	} 
+
+	@AfterClass
+	public void tearDown(){
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+
+	@Test
+	public void offerUpdate() throws Exception{
+		try
+		{
+			System.out.println("Login to Desidime..."); 
+			driver.get(url);
+			WebDriverWait wait = new WebDriverWait(driver,60);
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[contains(@class,'deal-off-percent')]/div[@class='getdeal ftl']/a")));
+
+			/********Scrolling HomePage **********************/
+			for(int i=1,j=300;i<=1;i++,j+=300)
+			{
+				dbconnectionutil.scrollDown(driver,j); 
+				System.out.println("Scrolling Down="+i);
+			}
+
+
+			List<WebElement> dealBoxShadow = driver.findElements(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']"));
+			List<WebElement> dealDescriptions = driver.findElements(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[@class='deal-dsp']/a"));
+			List<WebElement> dealTimes = driver.findElements(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[@class='promoblock']/div[@class='tgrid']/div[@class='tgrid-cell']/div[@class='promotime']/span"));
+			List<WebElement> dealStores = driver.findElements(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[@class='cf']/div[@class='deal-store ftl']/a"));
+			List<WebElement> dealPrices = driver.findElements(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[contains(@class,'deal-off-percent')]/div[@class='ftl dealpriceoff']"));
+			List<WebElement> dealImages = driver.findElements(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[contains(@class,'deal-box-image fpd_prod_img')]/a/img"));
+			List<WebElement> dealLinks = driver.findElements(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[contains(@class,'deal-off-percent')]/div[@class='getdeal ftl']/a"));
+
+			System.out.println("No. of DealsBox="+dealBoxShadow.size());
+			System.out.println("No. of Dealsdescription="+dealDescriptions.size());
+			System.out.println("No. of dealStores="+dealStores.size());
+			System.out.println("No. of dealTimes="+dealTimes.size());
+			System.out.println("No. of dealPrices="+dealPrices.size());
+			System.out.println("No. of dealImages="+dealImages.size());
+			System.out.println("No. of dealLinks="+dealLinks.size());
+  
+
+			for(int i=0;i<dealDescriptions.size();i++)
+			{   
+              /*  if(i>16)
+                {   
+                	dbconnectionutil.implicitWait(2, driver);
+                	scrollDown+=600;
+                	dbconnectionutil.scrollDown(driver,scrollDown); 
+                	System.out.println("Scrolling Down to get more offers....");
+                	
+                }*/
+                
+                //deal=driver.findElement(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li["+i+"]/div[@class='deal-box shadow']/div[@class='deal-dsp']/a"));
+				deal=dbconnectionutil.getElementWithIndex(driver,i, ".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[@class='deal-dsp']/a");
+                //dealTime=driver.findElement(By.xpath(".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li["+i+"]/div[@class='deal-box shadow']/div[@class='promoblock']/div[@class='tgrid']/div[@class='tgrid-cell']/div[@class='promotime']/span"));
+                dealTime=dbconnectionutil.getElementWithIndex(driver,i, ".//*[@id='deals-grid']/ul[@class='gridfix cf deals_grid']/li/div[@class='deal-box shadow']/div[@class='promoblock']/div[@class='tgrid']/div[@class='tgrid-cell']/div[@class='promotime']/span");
+				if(dealTime.getText().contains("hours")||dealTime.getText().contains("minutes")||dealTime.getText().contains("seconds"))
+				{   
+
+
+					System.out.println("Getting Offer No.="+i);
+					driver.findElement(By.xpath("//li[@class='subNav-home']/a[text()='Hot']")).click();
+					deal.click();
+
+					/********************Getting Offer Details *******************/
+
+					if(driver.findElements(By.xpath(".//*[@id='deal-detail-like-dislike-container']/div/div[1]/div[1]/h1")).size()>0)
+						{ dealName=driver.findElement(By.xpath(".//*[@id='deal-detail-like-dislike-container']/div/div[1]/div[1]/h1")).getText().replaceAll("[^a-zA-Z0-9&,_%\\s]", "");
+						  
+						}
+					else
+						dealName="NA";
+					System.out.println("Latest Deals="+dealName);
+					if(driver.findElements(By.xpath("//ul[@class='dalist']/li/span/a")).size()>0)
+					{ 
+						dealStore=driver.findElement(By.xpath("//ul[@class='dalist']/li/span/a")).getText();
+						if(dealStore.contains("Amazon"))
+							dealStore="@Amazon";
+						else if(dealStore.contains("Flipkart"))
+							dealStore="@Flipkart";
+						else if(dealStore.contains("Paytm"))
+							dealStore="@Paytm";
+						else
+							dealStore="@Others";
+					}
+					else
+						dealStore="NA";
+					System.out.println("Deal Store="+dealStore);
+					if(driver.findElements(By.xpath("//div[@class='dealprice']/span")).size()>0)
+						dealPrice=driver.findElement(By.xpath("//div[@class='dealprice']/span")).getText();
+					else
+						dealPrice="0";	  
+					System.out.println("Deal Price="+dealPrice);
+
+					if(driver.findElements(By.xpath("//div[@class='dealpercent']/span[@class='line-through']")).size()>0)
+						dealMRP=driver.findElement(By.xpath("//div[@class='dealpercent']/span[@class='line-through']")).getText();
+					else
+						dealMRP="0";	  
+					System.out.println("Deal MRP="+dealMRP);
+
+					if(driver.findElements(By.xpath("//div[@class='postblock']/div[@class='image-wrap pos-relative']/a[@class='dealpromoimage']/img[contains(@class,'media-image')]")).size()>0)
+						dealImage=driver.findElement(By.xpath("//div[@class='postblock']/div[@class='image-wrap pos-relative']/a[@class='dealpromoimage']/img[contains(@class,'media-image')]")).getAttribute("src");
+					else
+						dealImage="http://www.kalahandi.info/wp-content/uploads/2016/05/sorry-image-not-available.png";	  
+					System.out.println("Deal Image="+dealImage);
+
+					if(driver.findElements(By.xpath("//ul[@class='dalist']/li[1]/span")).size()>0)
+						dealViews=driver.findElement(By.xpath("//ul[@class='dalist']/li[1]/span")).getText().replaceAll("[^0-9]", "");
+					else
+						dealViews="NA";	  
+					System.out.println("Deal Views="+dealViews);
+
+					if(driver.findElements(By.xpath("//div[@class='postblock']/div[@class='image-wrap pos-relative']/div[1]/a")).size()>0)
+					{
+						
+						if(dealStore.contains("Amazon"))
+						{ 
+							dealLink=driver.findElement(By.xpath("//div[@class='postblock']/div[@class='image-wrap pos-relative']/div[1]/a")).getAttribute("href").concat("&tag=arwin10-21");
+							newDealLink=dealLink.replace("https://links.desidime.com/?ref=deals&url=","");
+						}
+						else if(dealStore.contains("Flipkart"))
+						{
+							dealLink=driver.findElement(By.xpath("//div[@class='postblock']/div[@class='image-wrap pos-relative']/div[1]/a")).getAttribute("href").concat("&affid=arsoftech");
+							newDealLink=dealLink.replace("https://links.desidime.com/?ref=deals&url=","");
+						}
+						else
+						{
+							dealLink=driver.findElement(By.xpath("//div[@class='postblock']/div[@class='image-wrap pos-relative']/div[1]/a")).getAttribute("href");
+							newDealLink=dealLink.replace("https://links.desidime.com/?ref=deals&url=","");
+						}
+					}
+					else
+					{
+						newDealLink="NA";
+					}
+					System.out.println("Deal Link="+newDealLink);
+					
+					if(driver.findElements(By.xpath("//ul[@class='list-details']/li/a")).size()>0)
+						dealCategory=driver.findElement(By.xpath("//ul[@class='list-details']/li/a")).getText();
+					else
+						dealCategory="NA";	  
+					System.out.println("Deal Category="+dealCategory);
+
+					
+					if(driver.findElements(By.xpath("//div[@class='postblock']/div[@class='mainpost postcontent']")).size()>0)
+					{   
+						if(driver.findElements(By.xpath("//div[@class='postblock']/div[@class='mainpost postcontent']/p[1]")).size()>0)
+						dealDescp=driver.findElement(By.xpath("//div[@class='postblock']/div[@class='mainpost postcontent']/p[1]")).getText()+"\n";
+						else
+					    dealDescp= "End of Description";	
+						if(driver.findElements(By.xpath("//div[@class='postblock']/div[@class='mainpost postcontent']/p[2]")).size()>0)
+						dealDescp=dealDescp.concat(driver.findElement(By.xpath("//div[@class='postblock']/div[@class='mainpost postcontent']/p[2]")).getText())+"\n";
+						else
+					    dealDescp= "End of Description";	
+						if(driver.findElements(By.xpath("//div[@class='postblock']/div[@class='mainpost postcontent']/p[3]")).size()>0)
+						dealDescp=dealDescp.concat(driver.findElement(By.xpath("//div[@class='postblock']/div[@class='mainpost postcontent']/p[3]")).getText())+"\n";
+						else
+						dealDescp= "End of Description";
+					}
+					else
+					{
+						dealDescp= "End of Description";
+					}
+					System.out.println("Deal Description="+dealDescp);
+					
+
+					dbconnectionutil.dbUpdate(dealName,dealStore,dealPrice,dealMRP,dealImage,newDealLink,dealViews,dealDescp,dealCategory);	 
+					driver.navigate().to(url); 
+
+				} 			
+
+			}
+
+			System.out.println("Inserted All offers successfully.");
+			dbconnectionutil.dbClose();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception occured:"+e);
+			System.out.println("Offers Insertion Failed.");
+			e.printStackTrace();
+		}
+	}
+
+
+}
