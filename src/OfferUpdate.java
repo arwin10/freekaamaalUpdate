59eb2aa2e7fa33eab233a0802f3434be4d938534
arwin10 <arsoftech@gmail.com>
2019-08-21 15:51:04 +0300

    Update OfferUpdate.java

--- src/OfferUpdate.java
+++ src/OfferUpdate.java
@@ -76,8 +76,8 @@ public class OfferUpdate {
 			 /************ Declaring and initialising the Headless Browser **********/
 			 //driver = new HtmlUnitDriver();
 			 //driver.setJavascriptEnabled(true);			
-		     //System.setProperty("phantomjs.binary.path", "phantomjs.exe");		
-                      //driver = new PhantomJSDriver();	
+		     System.setProperty("phantomjs.binary.path", "phantomjs.exe");		
+                      driver = new PhantomJSDriver();	
 			
 
 			/*****-------------Local Execution------------- ***/
@@ -85,10 +85,10 @@ public class OfferUpdate {
 			//System.setProperty("webdriver.firefox.bin", "C:\\FirefoxPortable45ESR\\FirefoxPortable.exe");
 			//driver=new FirefoxDriver();
 
-			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-			ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.setHeadless(true);
-			driver = new ChromeDriver(chromeOptions);
+			//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+			//ChromeOptions chromeOptions = new ChromeOptions();
+			//chromeOptions.setHeadless(true);
+			//driver = new ChromeDriver(chromeOptions);
 
 			//Puts an Implicit wait, Will wait for 10 seconds before throwing exception
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
