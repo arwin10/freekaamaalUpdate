688614f4654927db3b6891b64bea589492169649
Arindam Sarkar <arindam.sarkar@cigniti.com>
2021-03-28 11:53:35 +0530

    Fixes offer insertion

--- src/OfferUpdate.java
+++ src/OfferUpdate.java
@@ -78,24 +78,24 @@ public class OfferUpdate {
 			 //driver = new HtmlUnitDriver();
 			 //driver.setJavascriptEnabled(true);			
 		     //System.setProperty("phantomjs.binary.path", "phantomjs.exe");		
-             driver = new PhantomJSDriver();	
+             //driver = new PhantomJSDriver();	
 			
 			/*****-------------Local Execution------------- ***/
 			//System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
 		    // System.setProperty("webdriver.firefox.bin", "C:\\FirefoxPortable45ESR\\FirefoxPortable.exe");
 			//driver=new FirefoxDriver();
 
-			//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 			
             //String serverurl = "http://selenium-ch:4444/wd/hub";
 		
-			//ChromeOptions chromeOptions = new ChromeOptions();
-			//chromeOptions.setHeadless(true);
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.setHeadless(true);
 		    //driver = new RemoteWebDriver(new URL(serverurl),chromeOptions);
 	
 			//ChromeOptions chromeOptions = new ChromeOptions();
 		    //chromeOptions.setHeadless(true);
-			//driver = new ChromeDriver(chromeOptions);
+			driver = new ChromeDriver(chromeOptions);
 
 			//Puts an Implicit wait, Will wait for 10 seconds before throwing exception
 			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
