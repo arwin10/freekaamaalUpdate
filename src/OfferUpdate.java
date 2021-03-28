f9fd886a6e3c110e290c5d4b363bd147d7855a80
Arindam Sarkar <arindam.sarkar@cigniti.com>
2021-03-28 12:02:52 +0530

    Fixes2 offer insertion

--- src/OfferUpdate.java
+++ src/OfferUpdate.java
@@ -85,7 +85,7 @@ public class OfferUpdate {
 		    // System.setProperty("webdriver.firefox.bin", "C:\\FirefoxPortable45ESR\\FirefoxPortable.exe");
 			//driver=new FirefoxDriver();
 
-			System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 			
             //String serverurl = "http://selenium-ch:4444/wd/hub";
 		
